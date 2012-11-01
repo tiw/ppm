a924a32ee8fce533d7e82cf710fd2210e69efc20
Ting Wang <tting.wang@gmail.com>
2012-11-01 22:09:17 +0800

    Adding, Editing and Deleting the todo.

--- module/Todo/src/Todo/Controller/TodoController.php
+++ module/Todo/src/Todo/Controller/TodoController.php
@@ -4,6 +4,9 @@ namespace Todo\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
+use Todo\Form\TodoForm;
+use Todo\Model\Todo;
+
 class TodoController extends AbstractActionController
 {
 
@@ -26,13 +29,76 @@ class TodoController extends AbstractActionController
 
     public function addAction()
     {
+        $form = new TodoForm();
+        $form->get('submit')->setValue('Add');
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $todo = new Todo();
+            $form->setInputFilter($todo->getInputFilter());
+            $form->setData($request->getPost());
+
+            if ($form->isValid()) {
+                $todo->exchangeArray($form->getData());
+                $this->getTodoTable()->saveTodo($todo);
+                return $this->redirect()->toRoute('todo');
+            }
+        }
+        return array('form' => $form);
     }
 
     public function editAction()
     {
+        $id = (int) $this->params()->fromRoute('id', 0);
+        if (!$id) {
+            return $this->redirect()->toRoute('todo', array(
+                'action' => 'add'
+            ));
+        }
+        $todo = $this->getTodoTable()->getTodo($id);
+
+        $form = new TodoForm();
+        $form->bind($todo);
+        $form->get('submit')->setAttribute('value', Edit);
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $form->setInputFilter($todo->getInputFilter());
+            $form->setData($request->getPost());
+
+            if ($form->isValid()) {
+                $this->getTodoTable()->saveTodo($form->getData());
+                return $this->redirect()->toRoute('todo');
+            }
+        }
+
+        return array(
+            'id' => $id,
+            'form' => $form,
+        );
     }
 
     public function deleteAction()
     {
+        $id = (int) $this->params()->fromRoute('id', 0);
+        if (!$id) {
+            return $this->redirect()->toRoute('todo');
+        }
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $del = $request->getPost('del', 'No');
+
+            if ($del == 'Yes') {
+                $id = (int) $request->getPost('id');
+                $this->getTodoTable()->deleteTodo($id);
+            }
+
+            return $this->redirect()->toRoute('todo');
+        }
+
+        return array(
+            'id' => $id,
+            'todo' => $this->getTodoTable()->getTodo($id)
+        );
     }
 }
---
+++ module/Todo/src/Todo/Form/TodoForm.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Todo\Form;
+
+use Zend\Form\Form;
+
+class TodoForm extends Form
+{
+    public function __construct($name = null)
+    {
+        parent::__construct('todo');
+        $this->setAttribute('method', 'post');
+        $this->add(array(
+            'name' => 'id',
+            'attributes' => array(
+                'type' => 'hidden',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'description',
+            'attributes' => array(
+                'type' => 'text',
+            ),
+            'options' => array(
+                'label' => 'Description',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'status',
+            'attributes' => array(
+                'type' => 'text'
+            ),
+            'options' => array(
+                'label' => 'Status',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'submit',
+            'attributes' => array(
+                'type' => 'submit',
+                'value' => 'Go',
+                'id' => 'submitbutton',
+            ),
+        ));
+    }
+}
