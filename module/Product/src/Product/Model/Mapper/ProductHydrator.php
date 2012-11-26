0424c18872c145b3920496ae007dc02fc2ed7ca8
Ting Wang <tting.wang@gmail.com>
2012-11-26 21:34:13 +0800

    Check if the category name is set before using it.

--- module/Product/src/Product/Model/Mapper/ProductHydrator.php
+++ module/Product/src/Product/Model/Mapper/ProductHydrator.php
@@ -16,7 +16,9 @@ class ProductHydrator extends ClassMethods
     //put your code here
     public function hydrate(array $data, $object)
     {
-        $object->writeCategoryName($data['category_name']);
+        if (isset($data['category_name'])) {
+            $object->writeCategoryName($data['category_name']);
+        }
         return parent::hydrate($data, $object);
     }
 }
